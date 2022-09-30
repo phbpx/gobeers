b348e7c7526f2c51c0e6b1775f99902e3f88165c
Paulo Bortolotti <bortolotti.paulo@gmail.com>
2022-09-30 18:12:47 -0300

    add kit
    Signed-off-by: Paulo Bortolotti <bortolotti.paulo@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,20 @@
 module github.com/phbpx/gobeers
 
 go 1.19
+
+require (
+	github.com/dimfeld/httptreemux/v5 v5.4.0
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.36.1
+	go.opentelemetry.io/otel v1.10.0
+	go.opentelemetry.io/otel/trace v1.10.0
+	go.uber.org/zap v1.23.0
+)
+
+require (
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/otel/metric v0.32.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+)
---
+++ kit/web/context.go
@@ -0,0 +1,75 @@
+package web
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
+)
+
+// ctxKey represents the type of value for the context key.
+type ctxKey int
+
+// key is how request values are stored/retrieved.
+const key ctxKey = 1
+
+// Values represent state for each request.
+type Values struct {
+	TraceID    string
+	Tracer     trace.Tracer
+	Now        time.Time
+	StatusCode int
+}
+
+// GetValues returns the values from the context.
+func GetValues(ctx context.Context) (*Values, error) {
+	v, ok := ctx.Value(key).(*Values)
+	if !ok {
+		return nil, errors.New("web value missing from context")
+	}
+	return v, nil
+}
+
+// GetTraceID returns the trace id from the context.
+func GetTraceID(ctx context.Context) string {
+	v, ok := ctx.Value(key).(*Values)
+	if !ok {
+		return "00000000-0000-0000-0000-000000000000"
+	}
+	return v.TraceID
+}
+
+// AddSpan adds a OpenTelemetry span to the trace and context.
+func AddSpan(ctx context.Context, spanName string, keyValues ...attribute.KeyValue) (context.Context, trace.Span) {
+	v, ok := ctx.Value(key).(*Values)
+	if !ok || v.Tracer == nil {
+		return ctx, trace.SpanFromContext(ctx)
+	}
+
+	ctx, span := v.Tracer.Start(ctx, spanName)
+	for _, kv := range keyValues {
+		span.SetAttributes(kv)
+	}
+
+	return ctx, span
+}
+
+// EndSpan allows the caller to defer the End method for the specified span.
+func EndSpan(span trace.Span) func(options ...trace.SpanEndOption) {
+	if span != nil {
+		return span.End
+	}
+	return func(options ...trace.SpanEndOption) {}
+}
+
+// SetStatusCode sets the status code back into the context.
+func SetStatusCode(ctx context.Context, statusCode int) error {
+	v, ok := ctx.Value(key).(*Values)
+	if !ok {
+		return errors.New("web value missing from context")
+	}
+	v.StatusCode = statusCode
+	return nil
+}
